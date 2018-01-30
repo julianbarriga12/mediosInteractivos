29625b91ef9ebfbf51998503b23fe45245364944
julianbarriga12 <35973401+julianbarriga12@users.noreply.github.com>
2018-01-30 14:46:07 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,125 @@
+function setup() { 
+createCanvas(500 ,266.17);   // Tamaño papel
+background(245,242,211);// Color papel
+	
+	
+strokeWeight(1/2);   // Tamaño pincel
+stroke(0,0,0);   // Color pincel // Sin borde
+fill(134,133,149); 
+ellipse(18.65,129.22,28.53*2,28.53*2);// Dibujar una elipse
+
+strokeWeight(2);   // Tamaño pincel
+stroke(0,0,0);   // Color pincel // Sin borde
+fill(245,242,211); 
+ellipse(324.58,116.06,30.79*2,30.79*2);// Dibujar una elipse
+
+noStroke();
+fill(245,242,211);
+rect(335.25,85.28,29.38,64.43);
+	
+noStroke();
+fill(255,255,255);
+beginShape();   // Iniciar una figura
+vertex(118.45,27.5);// Crear un vertice
+vertex(53.29,269.47);
+vertex(196.04,297.1); 
+endShape(CLOSE);	
+	
+strokeWeight(3);// Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(231,0,56,10);
+beginShape();   // Iniciar una figura
+vertex(118.45,27.5);// Crear un vertice
+vertex(53.29,269.47);
+vertex(196.04,297.1); 
+endShape(CLOSE);	
+	
+noStroke();
+fill(237,219,33); 
+beginShape();   // Iniciar una figura
+vertex(130.12,185.66);// Crear un vertice
+vertex(185.76,107.76);
+vertex(225.69,184.46); 
+endShape(CLOSE);
+
+strokeWeight(3); // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(0,0,0,0);
+beginShape();   // Iniciar una figura
+vertex(118.45,27.5);// Crear un vertice
+vertex(53.29,269.47);
+vertex(196.04,297.1); 
+endShape(CLOSE);	
+
+strokeWeight(1.5); // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+line(0,177.17,145.93,266.17);//Dibujar la linea
+	
+strokeWeight(1.5); // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+line(0,238.27,66.62,266.17);//Dibujar la linea
+	
+strokeWeight(3); // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+line(306.05,0,309.71,259.73);//Dibujar la linea
+	
+strokeWeight(2.2); // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+line(317.12,259.94,315.46,0);//Dibujar la linea
+	
+strokeWeight(1.5); // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+line(276.26,55.08,346.21,53.85);//Dibujar la linea
+	
+strokeWeight(1.5); // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+line(277.44,59.04,346.21,57.96);//Dibujar la linea
+
+strokeWeight(1.5); // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+line(282.7,233.03,346.05,231.77);//Dibujar la linea
+line(282.7,238.27,346.05,237);//Dibujar la linea
+line(282.7,243.5,346.05,242.23);//Dibujar la linea
+
+	
+strokeWeight(2);   // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(245,242,211); 
+ellipse(249.91,202.91,35.12*2,35.12*2);// Dibujar una elipse
+
+strokeWeight(1/4);   // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(245,242,211); 
+ellipse(249.91,202.91,32.59*2,32.59*2);// Dibujar una elipse
+	
+strokeWeight(1/4);   // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(245,242,211); 
+ellipse(249.91,202.91,31.32*2,31.32*2);// Dibujar una elipse
+	
+strokeWeight(1/4);   // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(245,242,211); 
+ellipse(249.91,202.91,30.24*2,30.24*2);// Dibujar una elipse
+	
+strokeWeight(1/4);   // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(245,242,211); 
+ellipse(249.91,202.91,26.42*2,26.42*2);// Dibujar una elipse
+	
+	
+strokeWeight(1/4);   // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(37,37,125); 
+ellipse(249.91,202.91,24.5*2,24.5*2);// Dibujar una elipse
+	
+strokeWeight(3.5);   // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(203,163,76); 
+ellipse(462.83,288.58,128.84,110.51);// Dibujar una elipse
+	
+strokeWeight(1/2);   // Tamaño pincel
+stroke(0,0,0);   // Color de la linea
+fill(131,170,141); 
+ellipse(481.1,275.81,84.88,71.43);// Dibujar una elipse
+} 
